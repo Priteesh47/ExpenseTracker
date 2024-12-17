4425e36f5c1d5bf5b7b3ea73a8dca6c84a73e04a
prite <prite@PRITEESH>
2024-12-17 11:49:41 +0545

    Add project files.

---
+++ ExpenseManagerApp1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseManagerApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExpenseManagerApp1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExpenseManagerApp1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
